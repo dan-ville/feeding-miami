32d0f7d4935f4668007a5268d3e98879e28bf343
dan-ville <d.villegas@visiontravel.net>
2020-12-03 16:50:02 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// let img = document.createElement('img'); 
+//             img.src = "/public/images/dual logo.png"; 
+//             document.getElementById('logos').appendChild(img); 
+//             down.innerHTML = "Image Element Added.";  
+
+// brook logo
+// cornerstone miami logo
+// doral vineyard logo
+// new hope nueva esperanza
+// pneuma
+// prc logo
+// share your heart logo
+// VDC LOGO
+// vous church logo
